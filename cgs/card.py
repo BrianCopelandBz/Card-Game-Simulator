9b2b9a946b199b854b45edc9d67bd66346e61467
Brian Copeland <brian.copeland@target.com>
2018-02-21 18:53:30 -0600

    Looks like __repr__ is the function I wanted back in the day

--- cgs/card.py
+++ cgs/card.py
@@ -14,6 +14,9 @@ class Card(object):
         self.pick_player = pick_player
         self.prompt = prompt
 
+    def __repr__(self)
+        return str(name)
+
     def __str__(self):
         return self.name
 
